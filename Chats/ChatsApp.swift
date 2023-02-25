4d6ee3634098295d4561dac8ce08ac14cbda18a6
Brad Bomkamp <bradbomkamp@gmail.com>
2023-02-24 21:01:11 -0600

    Initial Commit

---
+++ Chats/ChatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatsApp.swift
+//  Chats
+//
+//  Created by Brad Bomkamp on 2/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
