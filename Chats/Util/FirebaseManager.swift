d8517388f0ea46b1357e4cd1e0486be9283da9ad
Brad Bomkamp <bradbomkamp@gmail.com>
2023-02-27 20:03:03 -0600

    Fetching Current User From Firebase

---
+++ Chats/Util/FirebaseManager.swift
@@ -0,0 +1,31 @@
+//
+//  FirebaseManager.swift
+//  Chats
+//
+//  Created by Brad Bomkamp on 2/27/23.
+//
+
+import Foundation
+import Firebase
+import FirebaseStorage
+import FirebaseFirestore
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    let storage: Storage
+    let firestore: Firestore
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        
+        self.auth = Auth.auth()
+        self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
+        
+        super.init()
+    }
+    
+}
